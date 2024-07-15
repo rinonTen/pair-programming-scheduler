7818fb7d582a4b307b8c0e3c473463fb24685140
Rinon <rinon.ten@onja.org>
2024-07-15 16:05:40 +0300

    Add some submission status

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useActionState, useEffect, useState } from 'react';
+import { Suspense, useActionState, useEffect, useState } from 'react';
 import './App.css'
 import axios from 'axios';
 
@@ -21,23 +21,6 @@ function getCurrentDateFormatted() {
 
 const BACKEND_URL = "https://pair-programming-scheduler.onrender.com";
 
-const handleSubmit = async(_previousState: FormState, formData: FormData) => {
-    const name = formData.get('name');
-    const email = formData.get('email');
-    const date = formData.get('date');
-    const startTime = formData.get('from_time');
-    const endTime = formData.get('end_time');
-    const goal = formData.get('goal');
-    const data = { name, email, date, startTime, endTime, goal }
-    try {
-      await axios.post(`${BACKEND_URL}/api/send-data`, data);
-      console.log('Form data submitted successfully!');
-    } catch (error) {
-      console.error('Error submitting form data:', error);
-    }
-}
-
-
 const formInitialState = {
   name: '',
   date: getCurrentDateFormatted(),
@@ -50,9 +33,29 @@ const formInitialState = {
 type OnChangeEvent = { target: { name: string; value: string; }};
 
 function App() {
-  const [_formState, formSubmitAction] = useActionState(handleSubmit as any, formInitialState);
   const [formData, setFormData] = useState<FormState>(formInitialState);
+  const [submissionStatus, setSubmissionStatus] = useState<"initial" | "submitting" | "submitted" | "failed">('initial');
   const [developers, setDevelopers] = useState<string[]>([])
+
+  const handleSubmit = async(_previousState: FormState, formData: FormData) => {
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const date = formData.get('date');
+    const startTime = formData.get('from_time');
+    const endTime = formData.get('end_time');
+    const goal = formData.get('goal');
+    const data = { name, email, date, startTime, endTime, goal }
+    setSubmissionStatus("submitting");
+    try {
+      await axios.post(`${BACKEND_URL}/api/send-data`, data);
+      setSubmissionStatus("submitted");
+    } catch (error) {
+      setSubmissionStatus("failed");
+      console.error('Error submitting form data:', error);
+    }
+}
+
+const [_formState, formSubmitAction ] = useActionState(handleSubmit as any, formInitialState);
 
   useEffect(() => {
     const getData = async () => {
@@ -80,48 +83,50 @@ function App() {
     }
   };
   
-  const { email, name, from_time, end_time } = formData;
-  const isInValidEmail = !!email && !email.includes('onja.org') || !email.includes(name);
+  const { email, name, from_time } = formData;
+  const isInValidEmail = !!email && (!email.includes('onja.org') || !email.includes(name.toLowerCase()));
    
   return (
-    <>
-      <header>
-        <h1></h1>
-      </header>
+    <Suspense fallback={<div>Loading...</div>}>
       <main>
         <img className='logo' src='/onja_logo.svg' alt='Onja logo' />
         <h2>Hello wonderful friend <img className='wave' src='https://emoji.slack-edge.com/TSMBL8WEL/wave-animated/a7b45a3ed9e483da.gif' alt='' /></h2>
-        <h2>Fill in the form to schedule a pair programming session with Rinon</h2>
+        <h2>Please fill out the form to schedule a pair programming session with Rinon.</h2>
         <form action={formSubmitAction}>
           <fieldset className='name_select'>
             <label htmlFor="name">Select your name</label>
-            <select id='name' name='name'>
-              {developers.map(name => <option key={name} value={name}>{name}</option>)}
+            <select required={name === 'Select an option'} onChange={handleFieldChange} id='name' name='name'>
+              {["Select an option", ...developers].map(name => <option key={name} value={name}>{name}</option>)}
             </select>
           </fieldset>
           <fieldset className={`email_field ${isInValidEmail ? 'invalid' : ''}`}>
             <label htmlFor="email">Enter your Onja Mail?</label>
             <input required onChange={handleFieldChange} type='email' name='email'/>
-            {isInValidEmail? <span>You must use your Onja Mail</span>: ''}
+            {isInValidEmail ? <span>You must use your Onja email address.</span> : ''}
           </fieldset>
           <fieldset>
-            <label htmlFor="date">When do you plan to have a session?</label>
+            <label htmlFor="date">When do you plan to have a session? Please select a date and time</label>
             <div className='date_field'>
               <input required onChange={handleFieldChange} type='date' name='date'/>
               <div className='time_field'>
-                From <input required onChange={handleFieldChange} value={from_time} min="9:00" max="16:00" type='time' name='from_time'  /> 
-                to <input required onChange={handleFieldChange} min={from_time} max="16:30" value={end_time} type='time' name='end_time'  />
+                From <input required onChange={handleFieldChange} min="9:00" max="16:00" type='time' name='from_time'  /> 
+                to <input required onChange={handleFieldChange} min={from_time} max="16:30" type='time' name='end_time'  />
               </div>
             </div>
           </fieldset>
           <fieldset>
-            <label htmlFor="goal">What specific thing do you want to look into together? Leave blank if you don't have anything.</label>
+            <label htmlFor="goal">What specific thing do you want to look into together? Leave it blank if you don't have anything in mind.</label>
             <textarea onChange={handleFieldChange} name='goal' />
           </fieldset>
-          <button className='submit'>Submit</button>
+          <button disabled={submissionStatus === 'submitting'} className='submit'>Submit</button>
+          {submissionStatus === 'submitted' ? 
+              <p className='success_message'>Form data submitted successfully!</p> 
+              : submissionStatus === 'failed' 
+              ? <p className='failing_message'>An error has occured when submitting the form</p> : ''
+            }
         </form>
       </main>
-    </>
+    </Suspense>
   )
 }
 
